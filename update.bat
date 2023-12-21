a670200687c6860aae1863066b4bf46d4c28a24d
Raman Tank <ramantank04022002@gmail.com>
2023-12-21 17:05:12 +0530

    New Update

--- update.bat
+++ update.bat
@@ -1,10 +1,6 @@
 git checkout dev
 git add .
-<<<<<<< HEAD
 git commit -m "New Update"
-=======
-git commit -m "New Update"
->>>>>>> 56d2eb8f3fdf26dc0d175011245bd628fba3e6e3
 git checkout main
 git pull
 git merge dev
