56d2eb8f3fdf26dc0d175011245bd628fba3e6e3
Rohan Goyal <rohangoyal188@gmail.com>
2023-12-21 17:00:46 +0530

    New Update

--- update.bat
+++ update.bat
@@ -1,9 +1,6 @@
-REM Set the commit message or use "New Update" by default
-set commit_message=%1
-if "%commit_message%"=="" set commit_message=New Update
 git checkout dev
 git add .
-git commit -m "%commit_message%"
+git commit -m "New Update"
 git checkout main
 git pull
 git merge dev
