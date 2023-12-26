30c321b469f2f8df317182268224fc50c49c9d02
Raman Tank <ramantank04022002@gmail.com>
2023-12-26 20:55:56 +0530

    New Update

--- lib/screen/moments.dart
+++ lib/screen/moments.dart
@@ -15,42 +15,6 @@ class Moments extends StatefulWidget {
 }
 
 class _MomentsState extends State<Moments> {
-  String url = "https://s65zvs58-3002.inc1.devtunnels.ms";
-
-  List<String> tags = [
-    "mastiplay",
-    'hot',
-  ];
-
-  sendPost() async {
-    await http.post(
-      Uri.parse('$url/'),
-      body: {
-        'id': '1',
-        'title': 'Doodle',
-        'description': 'A book about a doodle',
-        'imageUrl': 'Image URl HERE',
-        'likes': 3,
-        'tags': tags,
-      },
-    );
-  }
-
-  List<MomentsModel> momentsModel = [];
-
-  getAllMoments() async {
-    final response = await http.get(Uri.parse("$url/moments"), headers: {
-      "Accept": "application/json",
-    });
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      momentsModel = data['moments']
-          .map<MomentsModel>((json) => MomentsModel.fromJson(json))
-          .toList();
-      print(momentsModel.length);
-      setState(() {});
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -227,34 +191,29 @@ class _MomentsState extends State<Moments> {
                       ),
                       Row(
                         children: [
-                          GestureDetector(
-                            onTap: () {
-                              sendPost();
-                            },
-                            child: Container(
-                              height: height / 20,
-                              width: width / 5,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30),
-                                border: Border.all(color: Colors.yellow),
-                              ),
-                              alignment: Alignment.center,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.person_add,
-                                    color: Colors.yellow,
-                                    size: height / 50,
-                                  ),
-                                  Text(
-                                    'Follow',
-                                    style: TextStyle(
-                                        fontSize: height / 60,
-                                        color: Colors.yellow),
-                                  ),
-                                ],
-                              ),
+                          Container(
+                            height: height / 20,
+                            width: width / 5,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              border: Border.all(color: Colors.yellow),
+                            ),
+                            alignment: Alignment.center,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.person_add,
+                                  color: Colors.yellow,
+                                  size: height / 50,
+                                ),
+                                Text(
+                                  'Follow',
+                                  style: TextStyle(
+                                      fontSize: height / 60,
+                                      color: Colors.yellow),
+                                ),
+                              ],
                             ),
                           ),
                           SizedBox(
