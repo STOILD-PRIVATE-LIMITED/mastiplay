90d4fd007763a1af5c87583d761e3401eaeb6526
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-01-01 14:33:14 +0530

    server addr changed

--- lib/webRTC/web_rtc.dart
+++ lib/webRTC/web_rtc.dart
@@ -11,9 +11,9 @@ import 'package:spinner_try/shivanshu/utils.dart';
 
 // TODO: set this to some global server
 // The below represents the server address of the server running the socket.io server
-// const String websocketUrl = "https://3.7.66.245:8080";
+const String websocketUrl = "https://3.7.66.245:8080";
 // const String websocketUrl = "https://192.168.9.64:8080";
-const String websocketUrl = "http://v9nm4hsv-8080.asse.devtunnels.ms";
+// const String websocketUrl = "http://v9nm4hsv-8080.asse.devtunnels.ms";
 Socket? socket;
 
 class WebRtcController {
