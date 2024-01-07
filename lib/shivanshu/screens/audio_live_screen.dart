860c22888720c07ca604a08462efb6140b4e65a7
Raman Tank <ramantank04022002@gmail.com>
2024-01-07 16:08:19 +0530

    New Update

--- lib/shivanshu/screens/audio_live_screen.dart
+++ lib/shivanshu/screens/audio_live_screen.dart
@@ -6,6 +6,8 @@ import 'package:spinner_try/shivanshu/utils.dart';
 import 'package:spinner_try/shivanshu/widgets/scroll_builder.dart';
 import 'package:spinner_try/webRTC/video_room.dart';
 
+import 'audio_page.dart';
+
 class AudioLive extends StatefulWidget {
   const AudioLive({
     super.key,
@@ -86,7 +88,7 @@ class _AudioLiveState extends State<AudioLive> {
                           navigatorPush(
                               context,
                               rooms[index].roomType == RoomType.audio
-                                  ? NewAudioRoom(
+                                  ? AudioPage(
                                       room: rooms[index],
                                     )
                                   : VideoRoom(
