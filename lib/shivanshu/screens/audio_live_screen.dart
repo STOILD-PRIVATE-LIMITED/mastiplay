b8fa24f2c31241f4b49e403cf2800aa405602e46
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-01-07 21:57:58 +0530

    New Update

--- lib/shivanshu/screens/audio_live_screen.dart
+++ lib/shivanshu/screens/audio_live_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spinner_try/shivanshu/models/room.dart';
-import 'package:spinner_try/shivanshu/models/webRTC/new_audio_room.dart';
+import 'package:spinner_try/shivanshu/screens/audio_page.dart';
 import 'package:spinner_try/shivanshu/screens/home_page.dart';
 import 'package:spinner_try/shivanshu/utils.dart';
 import 'package:spinner_try/shivanshu/widgets/scroll_builder.dart';
@@ -86,7 +86,7 @@ class _AudioLiveState extends State<AudioLive> {
                           navigatorPush(
                               context,
                               rooms[index].roomType == RoomType.audio
-                                  ? NewAudioRoom(
+                                  ? AudioPage(
                                       room: rooms[index],
                                     )
                                   : VideoRoom(
