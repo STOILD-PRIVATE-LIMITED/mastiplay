82474156d1af5fce7b75ceade37bb215f4b16e4d
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-01-08 13:49:56 +0530

    New Update

--- lib/shivanshu/models/globals.dart
+++ lib/shivanshu/models/globals.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spinner_try/shivanshu/utils.dart';
 import 'package:spinner_try/user_model.dart';
+import 'package:spinner_try/wave/wave_painter.dart';
 
 /// Instead of creating multiple instances of the same object
 /// I created then altogether here
@@ -21,12 +24,53 @@ late final SharedPreferences prefs;
 String chatServer = "https://rkncpgkx-3000.inc1.devtunnels.ms";
 
 // Moments Server --------------------------------------------------
-// const String momentsServer = "https://f82a-103-137-198-235.ngrok-free.app";
+const String momentsServer = "https://92v8xmgw-3007.inc1.devtunnels.ms";
 // const String momentsServer = "https://v9nm4hsv-3007.asse.devtunnels.ms";
 // const String momentsServer = "https://3.7.66.245:3007";
-const String momentsServer = "https://rkncpgkx-3007.inc1.devtunnels.ms";
+// const String momentsServer = "https://rkncpgkx-3007.inc1.devtunnels.ms";
 
 // WebRTC Server --------------------------------------------------
 // const String websocketUrl = "https://3.7.66.245:8080";
 // const String websocketUrl = "https://v9nm4hsv-8080.asse.devtunnels.ms";
 const String websocketUrl = "https://rkncpgkx-8080.inc1.devtunnels.ms";
+
+OverlayEntry? entry;
+Offset offset = Offset(220.sp, 600.sp);
+void showOverlay(context, Widget widget) {
+  entry = OverlayEntry(builder: (context) {
+    return Positioned(
+      left: offset.dx,
+      top: offset.dy,
+      child: GestureDetector(
+        onDoubleTap: () {
+          entry!.remove();
+        },
+        child: Card(
+          color: Colors.transparent,
+          elevation: 0,
+          child: Container(
+            height: 100.sp,
+            width: 100.sp,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(
+                color: Colors.transparent,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: EmptyEffect(
+              borderColor: Colors.pink,
+              outerMostCircleEndRadius: 100,
+              outerMostCircleStartRadius: 20,
+              numberOfCircles: 3,
+              child: widget,
+            ),
+          ),
+        ),
+      ),
+    );
+  });
+  final overlay = Overlay.of(context);
+  overlay.insert(entry!);
+}
